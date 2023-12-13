a23d8964b5248c9e6480a2a3de330b5f8c5d7519
Alexander Varlamov <varlamow.sas@gmail.com>
2023-12-13 21:19:45 +0300

    Moving scripts into src

--- src/tree_ring_watermark/__init__.py
+++ src/tree_ring_watermark/__init__.py
@@ -4,3 +4,4 @@ from ._check import check
 from ._detect import detect
 from ._get_noise import get_noise
 from .utils import set_org, get_org
+from inverse_stable_diffusion import InversableStableDiffusionPipeline
