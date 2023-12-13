98e59ab3b212edbd55a1cc4ff5c8560dae6c4f3a
Alexander Varlamov <varlamow.sas@gmail.com>
2023-12-13 21:39:43 +0300

    added another fix to __init__

--- src/tree_ring_watermark/__init__.py
+++ src/tree_ring_watermark/__init__.py
@@ -6,3 +6,4 @@ from ._detect import detect
 from ._get_noise import get_noise
 from .utils import set_org, get_org
 from .inverse_stable_diffusion import InversableStableDiffusionPipeline
+from .modified_stable_diffusion import ModifiedStableDiffusionPipeline
