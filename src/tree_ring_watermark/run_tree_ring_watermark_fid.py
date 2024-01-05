9e97fd5d2e523f962a6fef4eec48b77e2c21ec0c
Alphonsce <varlamov.al@phystech.edu>
2024-01-05 17:55:53 +0300

    option to calculate on pregenerated

--- src/tree_ring_watermark/run_tree_ring_watermark_fid.py
+++ src/tree_ring_watermark/run_tree_ring_watermark_fid.py
@@ -74,67 +74,68 @@ def main(args):
     # ground-truth patch
     gt_patch = get_watermarking_pattern(pipe, args, device)
 
-    for i in tqdm(range(args.start, args.end)):
-        seed = i + args.gen_seed
-        
-        current_prompt = dataset[i][prompt_key]
-        
-        ### generation
-        # generation without watermarking
-        set_random_seed(seed)
-        init_latents_no_w = pipe.get_random_latents()
+    if args.run_generation:
+        for i in tqdm(range(args.start, args.end)):
+            seed = i + args.gen_seed
+            
+            current_prompt = dataset[i][prompt_key]
+            
+            ### generation
+            # generation without watermarking
+            set_random_seed(seed)
+            init_latents_no_w = pipe.get_random_latents()
 
-        if args.run_no_w:
-            outputs_no_w = pipe(
+            if args.run_no_w:
+                outputs_no_w = pipe(
+                    current_prompt,
+                    num_images_per_prompt=args.num_images,
+                    guidance_scale=args.guidance_scale,
+                    num_inference_steps=args.num_inference_steps,
+                    height=args.image_length,
+                    width=args.image_length,
+                    latents=init_latents_no_w,
+                    )
+                orig_image_no_w = outputs_no_w.images[0]
+            else:
+                orig_image_no_w = None
+            
+            # generation with watermarking
+            if init_latents_no_w is None:
+                set_random_seed(seed)
+                init_latents_w = pipe.get_random_latents()
+            else:
+                init_latents_w = copy.deepcopy(init_latents_no_w)
+
+            # get watermarking mask
+            watermarking_mask = get_watermarking_mask(init_latents_w, args, device)
+
+            # inject watermark
+            init_latents_w = inject_watermark(init_latents_w, watermarking_mask,gt_patch, args)
+
+            outputs_w = pipe(
                 current_prompt,
                 num_images_per_prompt=args.num_images,
                 guidance_scale=args.guidance_scale,
                 num_inference_steps=args.num_inference_steps,
                 height=args.image_length,
                 width=args.image_length,
-                latents=init_latents_no_w,
+                latents=init_latents_w,
                 )
-            orig_image_no_w = outputs_no_w.images[0]
-        else:
-            orig_image_no_w = None
-        
-        # generation with watermarking
-        if init_latents_no_w is None:
-            set_random_seed(seed)
-            init_latents_w = pipe.get_random_latents()
-        else:
-            init_latents_w = copy.deepcopy(init_latents_no_w)
+            orig_image_w = outputs_w.images[0]
 
-        # get watermarking mask
-        watermarking_mask = get_watermarking_mask(init_latents_w, args, device)
-
-        # inject watermark
-        init_latents_w = inject_watermark(init_latents_w, watermarking_mask,gt_patch, args)
-
-        outputs_w = pipe(
-            current_prompt,
-            num_images_per_prompt=args.num_images,
-            guidance_scale=args.guidance_scale,
-            num_inference_steps=args.num_inference_steps,
-            height=args.image_length,
-            width=args.image_length,
-            latents=init_latents_w,
-            )
-        orig_image_w = outputs_w.images[0]
-
-        if args.with_tracking:
-            if i < args.max_num_log_image:
-                if args.run_no_w:
-                    table.add_data(wandb.Image(orig_image_no_w), wandb.Image(orig_image_w), current_prompt)
+            if args.with_tracking:
+                if i < args.max_num_log_image:
+                    if args.run_no_w:
+                        table.add_data(wandb.Image(orig_image_no_w), wandb.Image(orig_image_w), current_prompt)
+                    else:
+                        table.add_data(None, wandb.Image(orig_image_w), current_prompt)
                 else:
-                    table.add_data(None, wandb.Image(orig_image_w), current_prompt)
-            else:
-                table.add_data(None, None, current_prompt)
-        
-        image_file_name = image_files[i]['file_name']
-        if args.run_no_w:
-            orig_image_no_w.save(f'{no_w_dir}/{image_file_name}')
-        orig_image_w.save(f'{w_dir}/{image_file_name}')
+                    table.add_data(None, None, current_prompt)
+            
+            image_file_name = image_files[i]['file_name']
+            if args.run_no_w:
+                orig_image_no_w.save(f'{no_w_dir}/{image_file_name}')
+            orig_image_w.save(f'{w_dir}/{image_file_name}')
 
     ### calculate fid
     try:
@@ -187,6 +188,7 @@ if __name__ == '__main__':
     parser.add_argument('--gt_folder', default='fid_outputs/coco/ground_truth')
 
     parser.add_argument('--freq_log', default=5000, type=int)
+    parser.add_argument('--run_generation', action='store_true')
 
     # watermark
     parser.add_argument('--w_seed', default=999999, type=int)
